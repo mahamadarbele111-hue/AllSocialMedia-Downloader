998c211e6203f8e003464f120b17b99f5ad8f2f4
hamoka <mahamadarbele111@gmail.com>
2026-02-28 01:49:51 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,12 @@ const TikTokIcon = ({ size = 16, color = 'currentColor' }) => (
   </svg>
 );
 
+const XIcon = ({ size = 16, color = 'currentColor' }) => (
+  <svg width={size} height={size} viewBox="0 0 24 24" fill={color} xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.744l7.73-8.835L1.254 2.25H8.08l4.259 5.63L18.244 2.25zm-1.161 17.52h1.833L7.084 4.126H5.117L17.083 19.77z"/>
+  </svg>
+);
+
 const softStyles = `
   @import url('https://fonts.googleapis.com/css2?family=Noto+Kufi+Arabic:wght@400;600;700;800;900&family=Nunito:wght@400;600;700;800;900&display=swap');
   *, *::before, *::after { box-sizing: border-box; margin: 0; padding: 0; }
@@ -86,7 +92,7 @@ const platforms = [
   { id: 'youtube',   name: 'YouTube',   name_ku: 'یوتیوب',     icon: <Youtube />,   color: '#FF0000' },
   { id: 'instagram', name: 'Instagram', name_ku: 'ئینستاگرام', icon: <Instagram />, color: '#E1306C' },
   { id: 'facebook',  name: 'Facebook',  name_ku: 'فەیسبووک',   icon: <Facebook />,  color: '#1877F2' },
-  { id: 'twitter',   name: 'Twitter/X', name_ku: 'تویتەر',     icon: <Twitter />,   color: '#1DA1F2' },
+  { id: 'twitter',   name: 'Twitter/X', name_ku: 'تویتەر',     icon: <XIcon />,     color: '#000000' },
   { id: 'snapchat',  name: 'Snapchat',  name_ku: 'سناپچات',    icon: <Ghost />,     color: '#f5a623' },
 ];
 
